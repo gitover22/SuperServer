3d9c9d5cb110f1b2a4f0c11c141a098deef7ca57
gitover22 <qidizou88@gmail.com>
2024-03-28 16:34:02 +0800

    modify buffer.h

--- src/buffer/buffer.h
+++ src/buffer/buffer.h
@@ -1,5 +1,10 @@
 #ifndef BUFFER_H
 #define BUFFER_H
+
+
+
+#include <vector>
+#include <atomic>
 class Buffer{
     Buffer(int initBuffSize = 1024);
     ~Buffer() = default;
@@ -8,6 +13,34 @@ class Buffer{
     size_t PrependableBytes() const;
 
     const char * Peek() const;
+     void EnsureWriteable(size_t len);
+    void HasWritten(size_t len);
+
+    void Retrieve(size_t len);
+    void RetrieveUntil(const char* end);
+
+    void RetrieveAll() ;
+    std::string RetrieveAllToStr();
+
+    const char* BeginWriteConst() const;
+    char* BeginWrite();
+
+    void Append(const std::string& str);
+    void Append(const char* str, size_t len);
+    void Append(const void* data, size_t len);
+    void Append(const Buffer& buff);
+
+    ssize_t ReadFd(int fd, int* Errno);
+    ssize_t WriteFd(int fd, int* Errno);
+
+private:
+    char* BeginPtr_();
+    const char* BeginPtr_() const;
+    void MakeSpace_(size_t len);
+
+    std::vector<char> buffer_;
+    std::atomic<std::size_t> readPos;
+    std::atomic<std::size_t> writePos;
 };
 
 
