7bc1fc7ccd0d3e00d4e7c370986fae1d7c34fa01
gitover22 <qidizou88@gmail.com>
2024-03-29 18:33:26 +0800

    add httpresponse.h

--- src/http/http_response.h
+++ src/http/http_response.h
@@ -1,8 +1,40 @@
 #ifndef HTTP_RESPONSE_H
 #define HTTP_RESPONSE_H
+#include <bits/struct_stat.h>
 
 class HttpResponse{
+public:
+    HttpResponse();
+    ~HttpResponse();
+
+    void Init(const std::string& strDir,std::string& path,bool isKeepAlive = false,int code =-1);
+    void MakeResponse(Buffer& buff);
+    void UnmapFile();
+    char* File();
+    size_t FileLen() const;
+    void ErrorContent(Buffer& buff,std::string message);
+    int Code() const;
+private:
+    void AddStateLine(Buffer &buff);
+    void AddHeader(Buffer &buff);
+    void AddContent(Buffer &buff);
+
+    void ErrorHtml();
+    std::string GetFileType();
+    
+    int code;
+    bool isKeepAlive;
+    std::string path;
+    std::string srcDir;
+    char* mmFile;
+    struct stat mmFileStat;
+    static const std::unordered_map<std::string,std::string> SUFFIX_TYPE;
+    static const std::unordered_map<int,std::string> CODE_STATUS;
+    static const std::unordered_map<int,std::string> CODE_PATH;
+
+
 
 };
+
 
 #endif
