bc9ce1e3cfd8d7b00801245490e1a2a8b509f097
gitover22 <qidizou88@gmail.com>
2024-03-29 19:11:59 +0800

    add block_queue.h

---
+++ src/log/block_queue.h
@@ -0,0 +1,44 @@
+#ifndef BLOCK_QUEUE_H
+#define BLOCK_QUEUE_H
+
+#include <condition_variable>
+#include <bits/std_mutex.h>
+#include <deque>
+// 类模板 自定义的一个双端数组
+template<class T>
+class BlockDeque{
+public:
+    explicit BlockDeque(size_t MaxCapacity  = 1000);
+    ~BlockDeque();
+    void clear();
+    bool empty();
+    bool full();
+    void Close();
+    size_t size();
+    size_t capacity();
+    T front();
+    T back();
+    void push_back(const T& item);
+    void push_front(const T& item);
+    bool pop(T& item);
+    bool pop(T& item,int timeout);
+    void flush();
+private:
+    std::deque<T> deq;
+    size_t capacity;
+    std::mutex mtx;
+    bool isClose;
+    std::condition_variable condConsumer;
+    std::condition_variable condProducer;
+
+};
+
+
+
+
+
+
+
+
+
+#endif
