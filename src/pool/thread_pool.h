026136b3383b815fcb09792edd5d9fadaa534ec3
gitover22 <qidizou88@gmail.com>
2024-04-04 20:15:00 +0800

    modify thread_pool.h

--- src/pool/thread_pool.h
+++ src/pool/thread_pool.h
@@ -1,8 +1,24 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
+#include<mutex>
+#include<condition_variable>
 class ThreadPool{
-
+public:
+    explicit ThreadPool(size_t threadCount =8);
+    ThreadPool() = default;
+    ThreadPool(ThreadPool&&) = default;
+    ~ThreadPool();
+    template<typename F> void AddTask(F&& task);
+private:
+    struct Pool{
+        std::mutex mtx;
+        std::condition_variable cond;
+        bool isClosed;
+        std::queue<std::function<void()>> tasks;
+    };
+    std::shared_ptr<struct Pool> pool_;
+    
 };
 
 #endif
