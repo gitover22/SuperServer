2efbaccb5c6ce512aeeac43b5f436d000756a76d
gitover22 <qidizou88@gmail.com>
2024-04-10 19:17:55 +0800

    mod http_conn::write

--- src/http/http_conn.cpp
+++ src/http/http_conn.cpp
@@ -46,3 +46,43 @@ struct  sockaddr_in HttpConn::GetAddr() const{
 const char* HttpConn::GetIP()const {
     return inet_ntoa(addr.sin_addr);
 }
+
+int HttpConn::GetPort() const{
+    return addr.sin_port;
+}
+
+ssize_t HttpConn::read(int *saveErrno){
+    ssize_t len =-1;
+    do{
+        len =readBuff.ReadFd(fd,saveErrno);
+        if(len <=0){
+            break;
+        }
+    }while(isET);
+    return len;
+}
+ssize_t HttpConn::write(int* saveErrno){
+    ssize_t len = -1;
+    do{
+        len =writev(fd,iov,iovCnt);
+        if(len <=0){
+            *saveErrno=errno;
+            break;
+        }
+        if(iov[0].iov_len+iov[1].iov_len == 0) break;
+        else if(static_cast<size_t>(len) > iov[0].iov_len){
+            iov[1].iov_base = (uint8_t*) iov[1].iov_base + (len - iov[0].iov_len);
+            iov[1].iov_len -= (len - iov[0].iov_len);
+            if(iov[0].iov_len) {
+                writeBuff.RetrieveAll();
+                iov[0].iov_len = 0;
+            }
+        }
+        else {
+            iov[0].iov_base = (uint8_t*)iov[0].iov_base + len; 
+            iov[0].iov_len -= len; 
+            writeBuff.Retrieve(len);
+        }
+    }while(isET || ToWriteBytes() > 10240);
+    return len;
+}
